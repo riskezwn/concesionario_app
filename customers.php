240d1230588714c01c1a102db59179eff4e49736
Riskezwn <enriquegarciagasc@gmail.com>
2021-07-26 11:59:05 +0200

    add button to see seller customers

--- customers.php
+++ customers.php
@@ -2,7 +2,34 @@
 require_once('common/nav.php');
 ?>
 <main class="customers-grid">
-    <div>
+   <div class="buttonCustomers" id="buttonCustomers">Ver mis clientes</div>
+    <div class="allCustomers">
+        <div class="horizontal-card">
+            <div class="nombre">ID</div>
+            <div class="nombre">Nombre</div>
+            <div>Ciudad</div>
+            <div>Fecha alta</div>
+            <div>Vendedor</div>
+            <!-- <div class="stock"><?= $car['stock'] ?></div> -->
+        </div>
+        <?php
+        if ($customers = getCustomers($con)) :
+            while ($customer = mysqli_fetch_assoc($customers)) :
+        ?>
+            <div class="horizontal-card">
+                <div><?= $customer['id'] ?> </div>
+                <div class="nombre"><?= $customer['nombre'] ?> </div>
+                <div><?= $customer['ciudad'] ?></div>
+                <div><?= $customer['fecha_alta'] ?></div>
+                <div><?= $customer['vendedor'] ?></div>
+                <!-- <div class="stock"><?= $car['stock'] ?></div> -->
+            </div>
+        <?php
+            endwhile;
+        endif;
+        ?>
+    </div>
+    <div class="sellerCustomers">
         <div class="horizontal-card">
             <div class="nombre">ID</div>
             <div class="nombre">Nombre</div>
---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const BUTTON_CUSTOMERS = document.getElementById('buttonCustomers');
+
+BUTTON_CUSTOMERS.addEventListener('click', () => {
+    let allCustomers = document.getElementById('allCustomers')
+    let sellerCustomers = document.getElementById('sellerCustomers')
+
+    
+})
